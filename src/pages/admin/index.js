2fee4f7726c1383684213a1e26afefead0144711
wangxixi <18823382520@qq.com>
2019-08-15 10:28:05 +0800

    完善登录验证功能

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,18 +1,49 @@
 import React , { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { reqValidateUser } from '../../api'
 import data from '../../utils/store';
 import { getItem } from '../../utils/storage';
+import { message , Spin } from 'antd';
+
+import './index.less';
 
 export default class Admin extends Component {
-	render() {
+	state = {
+		isLoading:true
+	};
+	checkUserLogin =() => {
 		if(!data.user._id) {
 			const user = getItem();
 			if(!user) {
-				return <Redirect to="/login"/>;
+				this.props.history.replace('/login');
+				return true;
 			}
-			data.user = user;
-		}
 
+			//验证用户信息是否合法
+			reqValidateUser(user._id)
+				.then(() =>{
+					data.user = user;
+					//更新状态，显示admin页面
+					this.setState({
+						isLoading:false
+					})
+				})
+				.catch(() => {
+					//错误提示
+					message.error("请先登录",3)
+					this.props.history.replace('/login');
+				});
+			//需要loading
+			return true;
+		} else {
+			//不需要Loading
+			return  false
+		}
+	};
+	render() {
+
+		const isLoading = this.checkUserLogin();
+
+		if(isLoading) return <Spin className="admin-loading" tip="loading....." size="large"/>;
 
 		return <div>
 			Admin
