4e4fcfbabb8dbeb50dbac42dd9ddd072b1a62653
wangxixi <18823382520@qq.com>
2019-08-16 15:56:12 +0800

    完成退出登录功能

--- src/components/header-main/index.js
+++ src/components/header-main/index.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Button } from "antd";
+import { Button, Modal, message } from "antd";
 import { withRouter } from 'react-router-dom';
+import { removeItem } from '../../utils/storage'
+import data from '../../utils/store'
 
 
 
 import './index.less';
 
  class HeaderMain extends Component {
+
+ 	//退出登录功能
+ 	logout = () => {
+		Modal.confirm({
+			title:'你确定退出登录吗？',
+			okText:'确认',
+			cancelText:'取消',
+			onOk: () => {
+				// 移除用户数据 （本地 / 内存）
+				removeItem();
+				data.user = {};
+				// 退出登录成功
+				message.success('退出登录成功',3)
+				// 跳转到登录页面
+				this.props.history.replace('/login');
+			}
+
+		})
+	};
+
 	render() {
 		return <div className="header-main">
 			<div className="header-main-top">
-				<span>欢迎，xxx</span>
-				<Button type="link">退出</Button>
+				<span>欢迎，{data.user.username}</span>
+				<Button type="link" onClick={this.logout}>退出</Button>
 			</div>
 			<div className="header-main-bottom">
 				<h3>xx管理</h3>
